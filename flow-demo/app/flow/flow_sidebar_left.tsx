eae3ab90756c69ca35a8694657701742ea843f66
Jayabal Malaichamy <balan.ind@gmail.com>
2023-12-14 14:08:02 +0530

    update

--- flow-demo/app/flow/flow_sidebar_left.tsx
+++ flow-demo/app/flow/flow_sidebar_left.tsx
@@ -19,61 +19,19 @@ export default function FlowSidebarLeft({
 
   return (
     <aside className='border-r-2  text-sm w-64 h-screen text-black'>
-      <>
-        {/* <h3 className='text-xl mb-4 text-blue-900'>Nodes Panel</h3> */}
-        {/* <div
-          className='bg-white p-3 border-2  rounded cursor-move flex justify-center items-center text-blue-500 hover:bg-blue-500 hover:text-white transition-colors duration-200'
-          onDragStart={(event) => onDragStart(event, 'textnode')}
-          draggable
-        >
-          + New Entity
-        </div> */}
-        <button
-          className='btn_entity'
-          draggable
-          onDragStart={(event) => onDragStart(event, 'textnode')}
-        >
-          + New Entity
-        </button>
-        <input
-          type='text'
-          className='block w-full pt-2 px-3 pb-3 text-gray-700 border border-blue-300 rounded-lg bg-white focus:outline-none focus:border-blue-500'
-          value={nodeName}
-          onChange={handleInputChange}
-        />
-      </>
-      {/* {selectedNode ? (
-        //settings panel
-        <div>
-          <h3 className='text-xl mb-2 text-blue-900'>Update Node</h3>
-          <label className='block mb-2 text-sm font-medium text-blue-900'>
-            Node Name:
-          </label>
-          <input
-            type='text'
-            className='block w-full pt-2 px-3 pb-3 text-gray-700 border border-blue-300 rounded-lg bg-white focus:outline-none focus:border-blue-500'
-            value={nodeName}
-            onChange={handleInputChange}
-          />
-          <button
-            className='mt-4 bg-blue-500 text-white rounded p-2 hover:bg-blue-600'
-            onClick={() => setSelectedElements([])}
-          >
-            Go Back
-          </button>
-        </div>
-      ) : (
-        //node panel
-        <>
-          <div
-            className='bg-white p-3 border-2  rounded cursor-move flex justify-center items-center text-blue-500 hover:bg-blue-500 hover:text-white transition-colors duration-200'
-            onDragStart={(event) => onDragStart(event, 'textnode')}
-            draggable
-          >
-            + New Entity
-          </div>
-        </>
-      )} */}
+      <button
+        className='btn_entity'
+        draggable
+        onDragStart={(event) => onDragStart(event, 'textnode')}
+      >
+        + New Entity
+      </button>
+      <input
+        type='text'
+        className='block w-full pt-2 px-3 pb-3 text-gray-700 border border-blue-300 rounded-lg bg-white focus:outline-none focus:border-blue-500'
+        value={nodeName}
+        onChange={handleInputChange}
+      />
     </aside>
   );
 }
